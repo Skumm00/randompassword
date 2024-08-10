102d10802e39c30b3f527074f7139bdc07d69a23
Skand Bajpai <99634361+Skumm00@users.noreply.github.com>
2024-08-10 19:34:36 +0000

    Just added more features and options to my randompassword project and basicly finished the basics of it

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import getpass
 
+import random
+
+import string
+
 def create_user(username, password):
 
     #creates a user with username and password
@@ -28,7 +32,38 @@ def login(username, password):
         return False
 
     print(f"Welcome, {username}!")
+
+    
+    # Display custom message based on username
+    if username == "admin":
+        print("Welcome, Administrator!")
+    elif username == "guest":
+        print("Welcome, Guest!")
+    else:
+        print("Welcome, User!")
+    
+    # Display random quote
+    quotes = [
+        
+        "The only way to do great work is to love what you do.",
+        "Your time is limited, so don't waste it doing something you don't want to.",
+        "Life is the greatest test with only victors and losers.",
+        "Don't let others make desicions for you.",
+        "Stop Worrying so much, and start enjoying.",
+        "Dont Waste your time chasing someone elses dream. You can be who YOU want to.",
+        "Life is 100x harder then you imagine and things wont go your way. Thats the bueaty of it",
+
+    ]
+    random_quote = random.choice(quotes)
+    print("Random Quote:", random_quote)
+
     return True
+
+def generate_password(length=12):
+    # Generate a random password
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
 
 def main():
     #the main function we need for this password project
@@ -39,7 +74,8 @@ def main():
         print("\nChoose an option:")
         print("1. Create a new user")
         print("2. Login")
-        print("3. Exit")
+        print("3. Generate Password")
+        print("4. Exit")
 
         choice = input("Enter your choice: ")
 
@@ -52,6 +88,9 @@ def main():
             password = getpass.getpass("Enter password: ")
             login(username, password)
         elif choice == '3':
+            password = generate_password()
+            print("Generated Password:", password)
+        elif choice == '4':
             print("Exiting...")
             break
         else:
